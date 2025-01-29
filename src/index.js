a1e60324e2ca87d304c8f7bf4b2fd1e651da9cf0
Zuhair Ahmed <153817334+Zuhair0000@users.noreply.github.com>
2025-01-29 22:46:35 +0800

    new

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/NASA_APP">
+    <BrowserRouter basename="/NASA_REACT_APP">
       <App />
     </BrowserRouter>
   </React.StrictMode>
